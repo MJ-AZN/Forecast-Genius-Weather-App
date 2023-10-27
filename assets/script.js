b1bbc2ef4e9d80bc77561b67ec824029b9fedae1
MJ-AZN <michaeljibarra63@gmail.com>
2023-10-27 13:29:13 -0700

    Script draft. figuring out how to fix the forecast section

---
+++ assets/script.js
@@ -0,0 +1,72 @@
+// Define Constants and Variables
+const queryURLCurrent = "https://api.openweathermap.org/data/2.5/weather?q=";
+const queryURLForecast = "https://api.openweathermap.org/data/2.5/forecast?q=";
+const APIKey = "&appid=8ccf05f501ea475aadec956092b99391";
+
+// Event Listener for Form Submission
+document.getElementById('cityForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const cityInput = document.getElementById('cityInput').value;
+
+    const apiUrlCurrent = `${queryURLCurrent}${cityInput}&units=imperial${APIKey}`;
+    const apiUrlForecast = `${queryURLForecast}${cityInput}&units=imperial${APIKey}`;
+
+    // Fetch Current Weather Data
+    fetch(apiUrlCurrent)
+        .then(response => response.json())
+        .then(data => {
+            displayCurrentWeather(data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+
+    // Fetch 5-Day Forecast Data
+    fetch(apiUrlForecast)
+        .then(response => response.json())
+        .then(data => {
+            displayForecast(data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+});
+
+// Display Current Weather Data
+function displayCurrentWeather(data) {
+    const cityName = data.name;
+    const date = new Date(data.dt * 1000);
+    const iconCode = data.weather[0].icon;
+    const temperature = data.main.temp;
+    const humidity = data.main.humidity;
+    const windSpeed = data.wind.speed;
+
+    document.querySelector('#currentWeather h3').textContent = cityName;
+    document.querySelector('#currentWeather p:nth-child(2)').textContent = `Date: ${date.toDateString()}`;
+    document.querySelector('#currentWeather img').src = `https://openweathermap.org/img/w/${iconCode}.png`;
+    document.querySelector('#currentWeather p:nth-child(4)').textContent = `Temperature: ${temperature}°F`;
+    document.querySelector('#currentWeather p:nth-child(5)').textContent = `Humidity: ${humidity}%`;
+    document.querySelector('#currentWeather p:nth-child(6)').textContent = `Wind Speed: ${windSpeed} mph`;
+}
+
+// Display 5-Day Forecast Data
+function displayForecast(data) {
+    console.log("Displaying Forecast Data", data);
+    const forecastDays = data.list.slice(0, 5); // Get the first 5 forecast items
+
+    forecastDays.forEach((day, index) => {
+        const date = new Date(day.dt * 1000);
+        const iconCode = day.weather[0].icon;
+        const temperature = day.main.temp;
+        const windSpeed = day.wind.speed;
+        const humidity = day.main.humidity;
+
+        const forecastElement = document.querySelector(`#forecast .forecast-day:nth-child(${index + 2})`);
+        forecastElement.querySelector('h3').textContent = `Date ${index + 1}`;
+        forecastElement.querySelector('img').src = `https://openweathermap.org/img/w/${iconCode}.png`;
+        forecastElement.querySelector('p:nth-child(1)').textContent = `Temperature: ${temperature}°C`;
+        forecastElement.querySelector('p:nth-child(2)').textContent = `Wind Speed: ${windSpeed} m/s`;
+        forecastElement.querySelector('p:nth-child(3)').textContent = `Humidity: ${humidity}%`;
+    });
+}
